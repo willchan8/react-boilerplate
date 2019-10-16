03b4bb3cc8e32370c299c0a500c3b1f112d51f87
William Chan <wchan1992@gmail.com>
2019-10-16 12:31:12 -0700

    Update server port variable

--- server/index.js
+++ server/index.js
@@ -3,8 +3,8 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const app = express();
 
-const PORT = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, '/../client/dist')));
 
-app.listen(PORT, () => console.log(`Listening on PORT ${PORT}...`));
+app.listen(port, () => console.log(`Listening on PORT ${port}...`));
